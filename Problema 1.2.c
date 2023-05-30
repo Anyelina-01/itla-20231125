87b1f3e751af7b5683a5ec7b4bb9a5a5922068e6
Anyelina-01 <134117340+Anyelina-01@users.noreply.github.com>
2023-05-30 18:38:58 -0400

    Problemas del cap 1 del libro de F.C

--- Problema 1.2.c
+++ Problema 1.2.c
@@ -1,17 +1,18 @@
-#include <stdio.h> 
+#include <stdio.h>
+/* Invierte datos
+El programa, al recibir como dato un conjunto de entrada, invierte el orden de los mismos cuando los imprime.
 
-/* Promedio de sueldos. 
-El programa, al recibir como datos seis sueldos de un empleado, calcula tanto el ingreso total como el promedio mensual.
+  A, B, C, D: variables de tipo entero. */
 
-CLA: variable de tipo entero.
-SU1, SU2, SU3, SU4, SU5, SU6, ING, PRO: variables de tipo real. */ 
 int main (void)
-{ 
-int CLA;
-float SU1, SU2, SU3, SU4, SU5, SU6, ING, PRO; 
-printf("Ingrese la clave del empleado y los 6 sueldos: \n"); 
-scanf("%d %f %f %f %f %f %f", &CLA, &SU1, &SU2, &SU3, &SU4, &SU5, &SU6); 
-ING = (SU1 + SU2 + SU3 + SU4 + SU5 + SU6);
-PRO = ING / 6;
-printf ("\n %d   %5.2f  %5.2f", CLA, ING, PRO);
+{
+int A, B, C, D;
+printf("Ingrese cuatro datos de tipo entero: ");
+scanf("%d %d %d %d", &A, &B, &C, &D);
+printf("\n %d %d %d %d ", D, C, B, A);
 }
+
+
+
+
+
